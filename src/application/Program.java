77114006d1b8de48b5e363d791223eb9f6a01b97
Thiago Lima <tlima7601@gmail.com>
2025-02-16 13:38:44 -0300

    Inserindo na tabela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,58 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement ps = null;
 
         try {
             conn = DB.getconnection();
-            st = conn.createStatement();
 
-            rs = st.executeQuery("select * from department");
+            // ps = conn.prepareStatement(
+            //         "INSERT INTO SELLER" +
+            //                 "(NAME, EMAIL, BIRTHDATE, BASESALARY, DEPARTMENTID)" +
+            //                 "VALUES" +
+            //                 "(?, ?, ?, ?, ?)",
+            //                 Statement.RETURN_GENERATED_KEYS);
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + " | " + rs.getString("Name"));
+            // ps.setString(1, "Joao");
+            // ps.setString(2, "Joao@gmail.com");
+            // ps.setDate(3, new Date(sdf.parse("18/03/2003").getTime()));
+            // ps.setDouble(4, 3400.00);
+            // ps.setInt(5, 2);
+
+            ps = conn.prepareStatement("insert into department (name) values ('d1'), ('d2')",
+                Statement.RETURN_GENERATED_KEYS);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = ps.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! ID = " + id);
+                }
+            }else{
+                System.out.println("No rows affected! ");
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        } catch (SQLException s) {
+            s.printStackTrace();
         }finally{
-            DB.closeResultSet(rs);
-            DB.closeStatment(st);
+            DB.closeStatment(ps);
             DB.closeConnection();
         }
     }
